dbc43a07c424f12e0e1acac67ebd692da6283470
Dev Pokhariya <pokhriya.dev@gmail.com>
2020-06-17 17:25:20 +0530

    Script update on 06/17/2020-05:25:20 PM IST

--- route53/resource.tf
+++ route53/resource.tf
@@ -3,7 +3,7 @@ resource "aws_route53_zone" "primary" {
 }
   resource "aws_route53_record" "public_a_record" {
     zone_id = aws_route53_zone.primary.id
-    name    = "${format("%s.%s", element(var.a_subdomain_list, count.index), var.domain)}"
+    name    = "${format("%s.%s", element(var.a_subdomain_list, count.index), var.zone_name)}"
     type    = "A"
     ttl     = "300"
     records = flatten(["${split(",", lookup(var.a_subdomain_map, element(var.a_subdomain_list, count.index)))}"])  
@@ -12,7 +12,7 @@ resource "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "public_cname_record" {
   zone_id = aws_route53_zone.primary.id
-  name    = "${format("%s.%s", element(var.cname_subdomain_list, count.index), var.domain)}"
+  name    = "${format("%s.%s", element(var.cname_subdomain_list, count.index), var.zone_name)}"
   type    = "CNAME"
   ttl     = "5"
   records = ["${lookup(var.cname_subdomain_map, element(var.cname_subdomain_list, count.index))}"]
