1f358f208ac902a0c875db294b71751b53f72e13
Dev Pokhariya <pokhriya.dev@gmail.com>
2020-06-17 15:05:02 +0530

    Script update on 06/17/2020-03:05:02 PM IST

--- route53/resource.tf
+++ route53/resource.tf
@@ -2,11 +2,11 @@ resource "aws_route53_zone" "primary" {
   name = "paytm.com"
 }
   resource "aws_route53_record" "public_a_record" {
-    zone_id = "${aws_route53_zone.primary.id}"
+    zone_id = aws_route53_zone.primary.id
     name    = "${format("%s.%s", element(var.a_subdomain_list, count.index), var.domain)}"
     type    = "A"
     ttl     = "300"
-    records = [split(",", lookup(var.a_subdomain_map, element(var.a_subdomain_list, count.index)))]
+    records = ["${split(",", lookup(var.a_subdomain_map, element(var.a_subdomain_list, count.index)))}"]
     count   = length(var.a_subdomain_list)
   }
 
